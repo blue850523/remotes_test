5018fe742132068f8254db67c689454acdc86e1d
jay.li <>
2022-03-12 21:47:21 +0800

    deploy.sh 修改

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # 部署到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 # 以這個專案來講就要改成這樣以下這樣
-git push -f https://github.com/blue850523/remotes_test.git master:gh-pages
+git push -f git@github.com:blue850523/remotes_test.git master:gh-pages
 
 cd -
